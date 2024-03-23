ffb7d043c1d284d1f638ca977552afca695cd4e1
rohanDave8 <rohan8dave@gmail.com>
2024-03-23 17:50:25 -0500

    Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-black p-4 flex justify-between items-center">
+      <div className="flex items-center">
+        <Link href="/" className="text-white font-bold text-xl">Aegisteel</Link>
+      </div>
+      <div className="flex items-center">
+        <button className="bg-orange-600 text-white font-bold py-2 px-4 rounded">
+          Login with Metamask
+        </button>
+      </div>
+    </nav>
+  );
+};
