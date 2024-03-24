41b26d3b90998296542157a44de1adb3b686eb88
vk1815918 <viswaretaskotra@gmail.com>
2024-03-23 23:32:00 -0500

    updated the color of the button on homepage

--- app/homepage/introductionPage.tsx
+++ app/homepage/introductionPage.tsx
@@ -26,7 +26,7 @@ export default function Homepage() {
       </p>
       <div className="flex flex-col items-center fade-in fade-in-3">
         <textarea className="w-64 h-20 px-4 py-2 border-2 border-gray-300 bg-white text-black rounded-lg shadow-lg focus:outline-none focus:border-indigo-500 resize" placeholder="Enter your text here" onChange={handleChange}></textarea>
-        <button onClick={handleSubmit} className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded fade-in-4">Submit</button>
+        <button onClick={handleSubmit} className="mt-8 bg-purple-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded fade-in-4">Submit</button>
       </div>
     </div>
   </div>
