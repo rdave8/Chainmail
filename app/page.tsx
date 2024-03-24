8e6123dc9da1ce265002235a2970193b24b060c0
vk1815918 <viswaretaskotra@gmail.com>
2024-03-23 20:12:33 -0500

    slight changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Homepage from "./homepage/introductionPage";
 
 export default function Home() {
   return (
     <main>
       <Navbar></Navbar>
+      <Homepage></Homepage>
     </main>
   );
 }
