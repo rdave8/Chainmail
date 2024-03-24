f3145d73c9dee64d838b63084633f596c35dff1a
vk1815918 <viswaretaskotra@gmail.com>
2024-03-23 22:29:11 -0500

    made the landing page realllly good

--- app/homepage/introductionPage.tsx
+++ app/homepage/introductionPage.tsx
@@ -1,13 +1,34 @@
-import Link from 'next/link';
+'use client'
+import React, { useState, ChangeEvent } from 'react';
 
 export default function Homepage() {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    console.log(inputValue);
+  }
+
   return (
-    <div className="h-screen flex justify-center items-center bg-black">
-      <div className="text-center">
-        <h1 className="text-white font-bold text-3xl">
-          Aegis - the most advanced AI model to optimize smart contracts
-        </h1>
+    <div className="h-screen flex flex-col justify-center items-center bg-black relative">
+    <video autoPlay loop muted className="absolute z-0 min-w-full min-h-full w-auto h-auto object-cover">
+      <source src="background_loop_video - Made with Clipchamp (1).mp4" type="video/mp4" />
+    </video>
+    <div className="text-center absolute z-10">
+      <h1 className="text-white font-bold text-9xl mb-8 font-serif fade-in fade-in-1">
+        Aegis
+      </h1>
+      <p className="text-gray-400 text-4xl italic font-serif mb-16 fade-in fade-in-2">
+        The most advanced AI model to optimize smart contracts to date
+      </p>
+      <div className="flex flex-col items-center fade-in fade-in-3">
+        <textarea className="w-64 h-20 px-4 py-2 border-2 border-gray-300 bg-white text-black rounded-lg shadow-lg focus:outline-none focus:border-indigo-500 resize" placeholder="Enter your text here" onChange={handleChange}></textarea>
+        <button onClick={handleSubmit} className="mt-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded fade-in-4">Submit</button>
       </div>
     </div>
+  </div>
   );
 }
